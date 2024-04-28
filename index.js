8a4433a03f4837dc2768f71f642b7a4e8ff4b4ef
David-Salihu <davidsalihu19@gmail.com>
2024-04-28 17:33:44 +0100

    Create new database client

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 import bodyParser from "body-parser";
 import express from "express"
+import pg from "pg"
+import env from "dotenv"
+
+
+// Loads .env file contents into process.env
+env.config();
+
+// create a new postgres database client
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
+
+// connect to the postgres database
+db.connect();
 
 const app = new express();
 const port = 3000;
