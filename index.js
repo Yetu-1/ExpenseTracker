1793a86d486d433ccb7dd8444c3da4a90d304976
David-Salihu <davidsalihu19@gmail.com>
2024-04-28 17:48:14 +0100

    Add setup files and create new db client

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import bodyParser from "body-parser";
 import express from "express"
 import pg from "pg"
 import env from "dotenv"
+import passport from "passport";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import { google } from "googleapis"
 
 
 // Loads .env file contents into process.env
@@ -22,6 +26,8 @@ db.connect();
 const app = new express();
 const port = 3000;
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -29,6 +35,46 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
+
+
+passport.use(
+    "google",
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+       },
+      async (accessToken, refreshToken, profile, cb) => {
+        await listOfLabels(accessToken);
+        await getLatestMessage(accessToken);
+        try {
+          const result = await db.query("SELECT * FROM users WHERE email = $1", [
+            profile.email,
+          ]);
+          if (result.rows.length === 0) {
+            const newUser = await db.query(
+              "INSERT INTO users (email, password) VALUES ($1, $2)",
+              [profile.email, "google"]
+            );
+            return cb(null, newUser.rows[0]);
+          } else {
+            return cb(null, result.rows[0]);
+          }
+        } catch (err) {
+          return cb(err);
+        }
+      }
+    )
+  );
+  passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
+  
+  passport.deserializeUser((user, cb) => {
+    cb(null, user);
+  });
 
 app.listen(port, ()=> {
     console.log(`Server running on port ${port}`);
