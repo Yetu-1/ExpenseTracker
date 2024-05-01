7c50b4841069944a9448f8eb9a1f5d160e3bf918
David-Salihu <davidsalihu19@gmail.com>
2024-05-01 08:39:18 +0100

    modularize project

--- index.js
+++ index.js
@@ -5,31 +5,13 @@ import env from "dotenv"
 import passport from "passport";
 import GoogleStrategy from "passport-google-oauth2";
 import session from "express-session";
-import { google } from "googleapis"
-import * as cheerio from 'cheerio';
+import { getLatestMessage, listOfLabels } from "./services/emailparser.js";
 
-// TODO :
-/**
- * display first name, surname user, and Image from gmail profile (console.log(req.user))
- */
 // global scope variables
-let mailBody ='';
 let userProfile = {};
 
 // Loads .env file contents into process.env so we can have access to the variables
 env.config();
-
-// create a new postgres database client
-const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-});
-
-// connect to the postgres database
-db.connect();
 
 const app = new express();
 const port = 3000;
@@ -94,105 +76,6 @@ app.get("/home", (req, res) => {
     res.send(mailBody)
   }
 });
-
-async function listOfLabels(accessToken) {
-
-  try{ 
-    const oauth2Client = new google.auth.OAuth2();
-    oauth2Client.setCredentials({ access_token: accessToken });
-
-    const gmail = google.gmail({version: 'v1', auth: oauth2Client});
-  
-    const response = await gmail.users.labels.list({
-      userId: "me",
-    });
-  
-    const labels = response.data.labels;
-
-    if(!labels || labels.length == 0){
-      console.log("No labels were found!");
-    }else {
-      console.log("Labels: ");
-      labels.forEach((label) => {
-        console.log(`- ${label.name}`); 
-      });
-    }
-
-  }catch(err) {
-    console.log("Error fetching labels", err);
-  }
-}
-
-async function getLatestMessage(accessToken){
-  try{ 
-    const oauth2Client = new google.auth.OAuth2();
-    oauth2Client.setCredentials({ access_token: accessToken });
-
-    const gmail = google.gmail({version: 'v1', auth: oauth2Client});
-  
-    const response = await gmail.users.messages.list({
-      userId: "me",
-      maxResults: 1,
-    });
-  
-    let latestMessageId = response.data.messages[0].id;
-    console.log("[MSG ID]: ", latestMessageId);
-
-    try{
-      const messageContent = await gmail.users.messages.get({
-        userId: "me",
-        id: latestMessageId,
-        format: "full",
-      });
-      const body = JSON.stringify(messageContent.data.payload.body.data);
-      // console.log("[MSG BASE64]", body);
-      mailBody = new Buffer.from(body, 'base64').toString();
-      //console.log("[MSG]: ", mailBody);
-      getTransactions(mailBody);
-      
-    }catch(err){
-      console.log("Error getting message by id!", err);
-    }
-
-  }catch(err) {
-    console.log("Error fetching messages!", err);
-  }
-}
-
-function getTransactions(rawHtml) {
-  // console.log(rawHtml);
-  let transactionInfo = {};
-
-  const $ = cheerio.load(rawHtml);
-  // const rawHtml = $('td'); // select Element
-
-  $('table').each((index, element) => {
-    // if it is the second table: ( which contains all the transaction informantion)
-    if(index === 1) {
-      // for each row in the table, 
-      $(element).find('tr').each((index, element) => {
-        let key = "";
-        let value = "";
-        // if there are 3 data cells (i.e it has a valid key value pair of the form  'x' ':' 'y')
-        if(length === 3) {
-          // extract the first and third data cells as the key value pair (second element is this ':' char )
-          $(element).find('td').each((index, element) => {
-            if(index === 0) {
-              key = $(element).text().trim(); // use trim() function to Remove the whitespace from the beginning and end of a string( the raw string had alot of white spaces)
-            }else if(index === 2){
-              value = $(element).text().trim();
-            }
-          });
-          // add key value pair to transactionInfo object
-          transactionInfo[key] = value;
-        }
-      });
-    }
-  });
-
-  console.log(transactionInfo);
-  return transactionInfo;
-}
 
 passport.use(
   "google",
