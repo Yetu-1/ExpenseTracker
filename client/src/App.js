26f384bca40e1e21a338b3c4c1fb0163a25bf997
David-Salihu <davidsalihu19@gmail.com>
2024-05-09 04:21:35 +0100

    fetch user data

--- client/src/App.js
+++ client/src/App.js
@@ -4,34 +4,34 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { UserContext } from "./components/UserContext";
 
 function App() {
-  // const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState({});
   
-  // useEffect(() => {
-  //   fetch("/api").then((response) => {
-  //     return response.json()
-  //   }).then((data) => {
-  //     setUserData(data)
-  //   })
-  // }, []); // Pass in an empty array so this runs only on the first render of the componenet
-  // function googleLogin() {
-  //   // event.preventDefault();
-  //   const str = `http://localhost:4000/auth/google`;
-  //   window.open(str, "_self");
-  // };
+  useEffect(() => {
+    fetch("/api").then((response) => {
+      return response.json()
+    }).then((data) => {
+      setUserData(data)
+    })
+  }, []); // Pass in an empty array so this runs only on the first render of the componenet
+  function googleLogin() {
+    // event.preventDefault();
+    const str = `http://localhost:4000/auth/google`;
+    window.open(str, "_self");
+  };
 
   // access the user object in UserContex.jsx
   const user = useContext(UserContext)
   return (
       <div>   
-        {/* <LoginForm /> 
+        <LoginForm /> 
         <h1>Home</h1> 
         <p>First Name: {(typeof userData.fName === 'undefined')? (<p>Loading...</p>) : userData.fName} </p>
         <p>Last Name: {(typeof userData.fName === 'undefined')? (<p>Loading...</p>) : userData.lName}</p>
         <img src={(typeof userData.fName === 'undefined')? "null": userData.img} alt="profile" /> 
-        <button onClick={() => googleLogin()}>
+        <button onClick={googleLogin}>
       Sign in with Google
-    </button> */}
-      {user?.loggedIn? <h1>loggedIn</h1> : <h1>loggedOut</h1>}
+    </button>
+      {/* {user?.loggedIn? <h1>loggedIn</h1> : <h1>loggedOut</h1>} */}
       </div>
   );
 }
