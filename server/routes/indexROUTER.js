c27a73b33456a385845fc8859c887aecee2ad944
David-Salihu <davidsalihu19@gmail.com>
2024-05-17 16:27:45 +0100

    Remove comments

--- server/routes/indexROUTER.js
+++ server/routes/indexROUTER.js
@@ -18,23 +18,24 @@ router.get("/register", (req, res) => {
 router.get("/home", async (req, res) => {
     //console.log(req.user.refreshtoken);
     const refreshToken = req.user.refreshtoken;
-    // get Latest messages and convert them into transaction objects
-    // const transactions = await getLatestMsgs(refreshToken);
-    // await addTransToDb(req.user, transactions);
-    // const {total_expenses, total_earnings} = await getTotalFinancials(req.user.id);
-    // console.log("TOTAL EXPENSES", total_expenses);
-    // console.log("TOTAL EARNINGS", total_earnings);
+    //get Latest messages and convert them into transaction objects
+    const transactions = await getLatestMsgs(refreshToken);
+    await addTransToDb(req.user, transactions);
+    const {total_expenses, total_earnings} = await getTotalFinancials(req.user.id);
+    console.log("TOTAL EXPENSES", total_expenses);
+    console.log("TOTAL EARNINGS", total_earnings);
 
-    // const last_10_transactions = await getLastTransactions(req.user.id);
-    // console.log("LAST 10 TRANSACTIONS: ")
-    // console.log(last_10_transactions);
-    // const balance = await getCurrentBalance(req.user.id);
-    // console.log("CURRENT BALANCE: ", balance);
+    const last_10_transactions = await getLastTransactions(req.user.id);
+    console.log("LAST 10 TRANSACTIONS: ")
+    console.log(last_10_transactions);
+    const balance = await getCurrentBalance(req.user.id);
+    console.log("CURRENT BALANCE: ", balance);
 
     if(req.isAuthenticated()) {
       //console.log(req.user);
-      res.render("home.ejs", {user: req.user})
+    //   res.render("home.ejs", {user: req.user})
     //   res.render("home.ejs",{fName: req.user.firstname, lName: req.user.lastname, img: req.user.picture});
+    res.sendStatus(200);
     }
 }); 
 
